85d2d8ef3e2ff6bee7998889387fc4228f7850af
Panpan <xpp2007@gmail.com>
2019-10-08 20:55:54 -0700

    .

--- app/backend/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-views = Blueprint("views", __name__, url_prefix='/', template_folder="../js/dashboard/build", static_folder="../js/dashboard/build/static", static_url_path='/static')
-api = Blueprint("api", __name__, url_prefix='/api/v1')
-
-from backend.views import *
-from backend.api import *
